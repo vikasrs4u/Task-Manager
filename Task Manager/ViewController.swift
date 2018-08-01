76091a65f8e603b4891d72c53ce1d9d87cd2afe7
Vikas Shetty <vikasrs4u@gmail.com>
2018-07-31 18:50:24 -0700

    Added the dark mode functionality

--- Task Manager/ViewController.swift
+++ Task Manager/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func darkModeClicked(_ sender: Any)
+    {
+        view.backgroundColor = UIColor.black
+        
+        // To get all the subviews, this will be a array of UIViews
+        let allSubViews = view.subviews
+        
+        // iterate through the array
+        for subView in allSubViews
+        {
+            // check if individual UIView is of type UILabel
+            if subView is UILabel
+            {
+                // If its a UILabel, the downcast it using as
+                let labelSubview = subView as! UILabel
+                
+                // Now change the c
+                labelSubview.textColor = UIColor.white
+                
+                UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
